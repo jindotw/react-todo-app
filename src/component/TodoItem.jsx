7ebaeaac3b64c948b27bf2e447e9a5f3cb9808a4
paul.lin <paul.lin@cherry.tv>
2021-10-07 22:35:10 +0800

    feat: set up the skeleton code for the todos app

--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function TodoItem(props) {
-  return <li>{props.todo.title}</li>
+  return <li><input type="checkbox" />{props.todo.title}</li>
 }
 
 export default TodoItem;
